bc4d355a14e37795497fa32995fd5bedab24d5f9
Maksim Komarov <komarovmu@gmail.com>
2023-04-03 23:49:08 +0100

    Seminar_1_FirstHomework

---
+++ Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Введите трехзначное число: ");
+        int number = int.Parse(Console.ReadLine());
+        int secondDigit = (number) % 10;
+        Console.WriteLine("3d number is: " + secondDigit);
+    }
+}
