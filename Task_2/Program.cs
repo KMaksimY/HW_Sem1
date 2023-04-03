1097c7eb581c3831d2185f6f50e54d64f6628a5b
Maksim Komarov <komarovmu@gmail.com>
2023-04-04 00:12:55 +0100

    Add task_2_HW-sem1

---
+++ Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Введите трехзначное число: ");
+        int number = int.Parse(Console.ReadLine());
+        if (number>99) {
+
+          int thirdDigit = (number/10) % 10;
+      
+        Console.WriteLine("3nd number is: " + thirdDigit);      
+          
+        }
+        else{
+        Console.WriteLine("Put the number more then 99");
+    }
+}
+}
